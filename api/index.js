41d9f29cd0da60e0808cdec892e836b317c41113
your-username <your-email>
2024-07-29 22:39:57 +0500

    connect db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+import router from "./routes/userRoute.js";
+import mongoose from "mongoose";
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("data base connected successfully"))
+  .catch((err) => console.log("somthing went wrong", err));
+app.use("/api", router);
 app.listen(PORT, () => console.log(`app is litening on port ${PORT}`));
