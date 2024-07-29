ee23cf51dfaa7f193bd1d145bfaaf7cbe8bcfe15
your-username <your-email>
2024-07-29 22:39:04 +0500

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,22 +2,22 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header className=" bg-gray-400 p-3 shadow-md">
+    <header className=" bg-gray-400 p-2 shadow-md">
       <nav className=" flex items-center justify-between max-w-6xl mx-auto">
-        <h1 className="text-sm font-medium sm:text-3xl flex flex-col sm:flex-row">
+        <h1 className="text-sm font-medium sm:text-2xl flex flex-col sm:flex-row">
           <span>Horizen</span>
           <span className="text-gray-600">Estate</span>
         </h1>
         <form
           action="#"
-          className=" flex items-center p-3 bg-slate-100 rounded-lg"
+          className=" flex items-center p-2 bg-slate-100 rounded-lg"
         >
           <input
             type="text"
             placeholder="search..."
             className=" bg-transparent py-0 sm:py-2 text-xl rounded-xl px-4 focus:outline-none w-24 sm:w-full"
           />
-          <FaSearch className="text-xl text-gray-500" />
+          <FaSearch className="text-xl text-gray-500 cursor-pointer" />
         </form>
         <ul className="flex items-center space-x-4">
           <li className=" hidden text-xl sm:inline hover:underline">
