58fa03ebb99c79e052cac91c817a639135b4e80a
your-username <your-email>
2024-07-30 16:58:45 +0500

    add  redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import userReducer from "./slices/userSlice";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "user",
+  storage,
+  verion: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 export default store;
